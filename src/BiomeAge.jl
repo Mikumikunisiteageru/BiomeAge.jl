fd658462509bcf08e6d8791f6f0735c1413c8698
Yu-Chang YANG <yang.yc.allium@gmail.com>
2023-04-21 16:13:25 +0800

    feat: add `read_lineages_from_tsv`

--- src/BiomeAge.jl
+++ src/BiomeAge.jl
@@ -2,10 +2,11 @@
 
 module BiomeAge
 
+using DelimitedFiles
 using Entropics
 using XLSX
 
-export read_lineages_from_xlsx
+export read_lineages_from_tsv, read_lineages_from_xlsx
 export add_up_age_distributions
 export TIMES
 
@@ -34,27 +35,36 @@ function med_wmusab(wmusab; w_as=:median)
 	return maxendist(a, b; median=m, mean=u, std=s)
 end
 
-function read_lineages_from_xlsx(file, sheet, 
-		rows, name_col, stem_cols, crown_cols)
-	table = XLSX.readxlsx(file)[sheet][:]
+function read_lineages_from_table(table, rows, name_col, stem_cols, crown_cols)
 	lineages = Lineage[]
 	for row = rows
 		name = table[row, name_col]
 		stem = med_wmusab(table[row, stem_cols])
 		crown = med_wmusab(table[row, crown_cols])
 		push!(lineages, Lineage(row, name, stem, crown))
-		println(row, ' ', name)
 	end
 	return lineages
 end
 
-function add_up_age_distributions(lineages, group, h)
+function read_lineages_from_tsv(filename; 
+		name_col=1, stem_cols=2:7, crown_cols=8:13)
+	table, _ = readdlm(filename, '\t', header=true)
+	return read_lineages_from_table(table, 
+		1 : size(table, 1), name_col, stem_cols, crown_cols)
+end
+
+read_lineages_from_xlsx(filename, sheet="Sheet1", 
+	rows=2:68, name_col=1, stem_cols=3:8, crown_cols=9:14) = 
+		read_lineages_from_table(XLSX.readxlsx(filename)[sheet][:], 
+			rows, name_col, stem_cols, crown_cols)
+
+function add_up_age_distributions(lineages, group=:crown, h=1.0)
 	@assert group in [:crown, :stem]
 	pdfs = Vector[]
 	for lineage = lineages
 		push!(pdfs, pdf(smooth(getproperty(lineage, group), h)).(TIMES))
 	end
 	return sum(pdfs)
-end	
+end
 
 end # module BiomeAge
