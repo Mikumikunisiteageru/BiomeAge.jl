1e34165447e07be9cca3bbf7eec3e4b31638849f
Yu-Chang YANG <yang.yc.allium@gmail.com>
2023-04-24 23:14:46 +0800

    feat: draw geologic time scale

--- test/ChEBLeaF_galileo.jl
+++ test/ChEBLeaF_galileo.jl
@@ -2,6 +2,7 @@
 
 using BiomeAge
 using Entropics
+using GeologicTime
 using PyPlot
 
 cd(joinpath(pkgdir(BiomeAge), "test"))
@@ -23,11 +24,35 @@ function print_name(name, ha=:left)
 end
 
 close()
-figure(figsize=(6.4, 6.4))
+figure(figsize=(6.4, 7.2))
+
+try ax1.remove() catch ; end
+# ax1 = PyPlot.axes([0.17, 0.94, 0.66, 0.05])
+ax1 = PyPlot.axes([103/4100, 0.94, 0.83 - 103/4100, 0.05])
+drawtimescale(ax1, 100, 0, [3, 4]; fontsize=8, texts = Dict(
+	"Cretaceous" => "Cretaceous", "Paleogene" => "Paleogene", 
+	"Neogene" => "Neogene", "Quaternary" => "Q", 
+	"Late Cretaceous" => "Late Cretaceous", "Paleocene" => "Paleoc.", 
+	"Eocene" => "Eocene", "Oligocene" => "Oligoc.", "Miocene" => "Miocene", 
+	"Pliocene" => "P", "Pleistocene" => "P"))
+
+try ax2.remove() catch ; end
+ax2 = PyPlot.axes([103/4100, 0.69, 0.83 - 103/4100, 0.24])
+crowns = sum(crown_ages)
+ax2.plot(TIMES, crowns)
+ax2.set_ylim([0, 3])
+ax2.tick_params(left=false, labelleft=false, right=true, labelright=true, 
+	bottom=false, labelbottom=false)
+ax2.set_xlim([100, 0])
+ax2.set_ylabel("Crown LAR / Ma\$^{-1}\$", labelpad=7)
+ax2.yaxis.set_label_position("right")
+
+try ax3.remove() catch ; end
+ax3 = PyPlot.axes([0.17, 0.06, 0.66, 0.62])
 STRETCH = -0.5 * maximum(maximum.(crown_ages[1:n-2] .+ crown_ages[3:n]))
 EPSILON = 0.1 * minimum(maximum.(crown_ages))
-LOWER = ((n+1) >> 1 + 1.3) * STRETCH
-UPPER = -1 * STRETCH
+LOWER = ((n+1) >> 1 + 0.9) * STRETCH
+UPPER = -0.5 * STRETCH
 SUBNOW = 0
 SUBOLD = 82
 TEXTSEP = 2
@@ -38,25 +63,24 @@ for (i, crown_age) = enumerate(crown_ages)
 	t = findlast(crown_age .>= EPSILON)
 	if i <= (n+1) >> 1
 		y = i * STRETCH
-		fill_between(TIMES[s:t], y .+ crown_age[s:t], y .- crown_age[s:t], 
+		ax3.fill_between(TIMES[s:t], y .+ crown_age[s:t], y .- crown_age[s:t], 
 			lw=0, fc=COLOR_RIN, alpha=0.75, zorder=-10)
-		plot([NOW, TIMES[s]], [y, y], "--", c="k", lw=0.4, zorder=-10)
-		text(SUBNOW - TEXTSEP, y, print_name(lineages[i].name, :left), 
+		ax3.plot([NOW, TIMES[s]], [y, y], "--", c="k", lw=0.4, zorder=-10)
+		ax3.text(SUBNOW - TEXTSEP, y, print_name(lineages[i].name, :left), 
 			ha="left", va="center", fontsize=8)
 	else
 		y = (i - n/2) * STRETCH
-		fill_between(TIMES[s:t], y .+ crown_age[s:t], y .- crown_age[s:t], 
+		ax3.fill_between(TIMES[s:t], y .+ crown_age[s:t], y .- crown_age[s:t], 
 			lw=0, fc=COLOR_LEN, alpha=0.75, zorder=-10)
-		plot([TIMES[t], OLD], [y, y], "--", c="k", lw=0.4, zorder=-10)
-		text(SUBOLD + TEXTSEP, y, print_name(lineages[i].name, :right), 
+		ax3.plot([TIMES[t], OLD], [y, y], "--", c="k", lw=0.4, zorder=-10)
+		ax3.text(SUBOLD + TEXTSEP, y, print_name(lineages[i].name, :right), 
 			ha="right", va="center", fontsize=8)
 	end
-	fill_between(TIMES[s:t], y .+ crown_age[s:t], y .- crown_age[s:t], 
+	ax3.fill_between(TIMES[s:t], y .+ crown_age[s:t], y .- crown_age[s:t], 
 		lw=0.4, ec="k", fc="none", alpha=1, zorder=-1)
-	plot(mean(lineages[i].crown), y, ".", c="k", ms=3)
+	ax3.plot(mean(lineages[i].crown), y, ".", c="k", ms=3)
 end
-xlim([0, 82])
-ylim([LOWER, UPPER])
-gca().invert_xaxis()
-yticks([])
-gca().set_position([0.17, 0.1, 0.66, 0.85])
+ax3.set_xlim([SUBOLD, SUBNOW])
+ax3.set_ylim([LOWER, UPPER])
+ax3.set_yticks([])
+ax3.set_xlabel("Time / Ma", labelpad=2)
