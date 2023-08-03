fc89cfbab1fdca344e62779d9f7b57ca034ae06f
Yu-Chang YANG <yang.yc.allium@gmail.com>
2023-08-03 16:30:56 +0800

    test: test with Aqua.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,13 @@
 # test/runtests.jl
 
-include("ChEBLeaF_db.jl")
+using Aqua
+using BiomeAge
+
+Aqua.test_ambiguities(BiomeAge)
+Aqua.test_unbound_args(BiomeAge)
+Aqua.test_undefined_exports(BiomeAge)
+Aqua.test_piracy(BiomeAge)
+Aqua.test_project_extras(BiomeAge)
+Aqua.test_stale_deps(BiomeAge)
+Aqua.test_deps_compat(BiomeAge)
+Aqua.test_project_toml_formatting(BiomeAge)
